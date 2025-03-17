54bb369e68e63f666970b54f81f045984f3bf5db
MantiCore2517 <107882760+MantiCore2517@users.noreply.github.com>
2025-03-17 03:08:57 +0300

    hotfix 0.001

--- src/ToDoList/ToDoList.jsx
+++ src/ToDoList/ToDoList.jsx
@@ -10,7 +10,6 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useDebounce } from "use-debounce";
-import { use } from "react";
 
 const formSchema = yup.object().shape({
 	search: yup
